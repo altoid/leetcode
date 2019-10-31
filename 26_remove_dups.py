0f3e9da1b3ad315a729f48f32cee68d557c182b4
Doug Tomm <dctomm@gmail.com>
2019-10-31 00:36:03 -0700

    works for example cases

--- 26_remove_dups.py
+++ 26_remove_dups.py
@@ -4,54 +4,24 @@ import unittest
 
 
 class Solution(object):
-    dup_limit = 2
-
     def removeDuplicates(self, arr):
-        """
-        Given a sorted array nums, remove the duplicates in-place such
-        that duplicates appeared at most twice and return the new length.
-
-        Do not allocate extra space for another array, you must do this
-        by modifying the input array in-place with O(1) extra memory.
-
-        :param arr:
-        :return:  length of de-duped array
-        """
-
-        if len(arr) <= self.dup_limit:
+        if len(arr) <= 1:
             return len(arr)
 
-        current_value = arr[0]
-        cv_count = 1
-        i = 1
+        # are we here?  then the array has at least 2 elements in it.
+        current = 0
+        next_value = 1
         array_len = len(arr)
-        while i < array_len:
-            if arr[i] != current_value:
-                current_value = arr[i]
-                cv_count = 1
-            else:
-                cv_count += 1
-                if cv_count > self.dup_limit:
-                    # find the next value not equal to the one we are pointing at.
-                    # from there to the end of the array, slide everything over (if
-                    # we can).
-                    j = i + 1
-                    while j < array_len:
-                        if arr[j] != current_value:
-                            break
-                        j += 1
-                    k = i
-                    while j < array_len:
-                        arr[k] = arr[j]
-                        k += 1
-                        j += 1
-                    array_len = k
 
-                    # don't increment i now - the value in that slot has been overwritten.
-                    continue
-            i += 1
+        while next_value < array_len:
+            while next_value < array_len and arr[current] == arr[next_value]:
+                next_value += 1
+            if next_value < array_len:
+                current += 1
+                arr[current] = arr[next_value]
+                next_value += 1
 
-        return array_len
+        return current + 1
 
 
 class MyTest(unittest.TestCase):
@@ -65,24 +35,35 @@ class MyTest(unittest.TestCase):
         self.assertEqual([1], arr[:result])
 
         arr = [1, 1]
-        result = 2
+        result = 1
         self.assertEqual(result, self.solution.removeDuplicates(arr))
-        self.assertEqual([1, 1], arr[:result])
+        self.assertEqual([1], arr[:result])
 
         arr = [1, 1, 1]
-        result = 2
+        result = 1
         self.assertEqual(result, self.solution.removeDuplicates(arr))
-        self.assertEqual([1, 1], arr[:result])
+        self.assertEqual([1], arr[:result])
 
     def test1(self):
         arr = [1, 1, 1, 2, 2, 3]
-        result = 5
-        self.assertEqual(result, self.solution.removeDuplicates(arr))
-        self.assertEqual([1, 1, 2, 2, 3], arr[:result])
+        control = [1, 2, 3]
+        expected = len(control)
+        result = self.solution.removeDuplicates(arr)
+        self.assertEqual(expected, result)
+        self.assertEqual(control, arr[:expected])
 
     def test2(self):
         arr = [0, 0, 0, 1, 1, 1, 1, 2, 3, 3]
-        expected = 7
+        control = [0, 1, 2, 3]
+        expected = len(control)
         result = self.solution.removeDuplicates(arr)
         self.assertEqual(expected, result)
-        self.assertEqual([0, 0, 1, 1, 2, 3, 3], arr[:expected])
+        self.assertEqual(control, arr[:expected])
+
+    def test_nodups(self):
+        arr = [0, 1, 2, 3]
+        control = [0, 1, 2, 3]
+        expected = len(control)
+        result = self.solution.removeDuplicates(arr)
+        self.assertEqual(expected, result)
+        self.assertEqual(control, arr[:expected])
