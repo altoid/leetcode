8d55f0637d930390dea1de306563c6af8951a580
Doug Tomm <dctomm@gmail.com>
2021-04-19 22:44:27 -0700

    make Solution class wrapper

--- april_2021_challenges/combination_sum_iv.py
+++ april_2021_challenges/combination_sum_iv.py
@@ -5,41 +5,49 @@
 import unittest
 
 
-def combination_sum(arr, target):
-    # numbers in arr are distinct
-
-    # for each n in arr, return combination_sum(arr, target - n)
-
-    # enumerate, don't generate
-
-    if target == 1:
-        if target in arr:
-            return 1
-        return 0
-
-    result = 0
-    if target in arr:
-        result = 1
+class Solution(object):
+    def __init__(self):
+        self.state = {}
+        
+    def combination_sum(self, arr, target):
+        # numbers in arr are distinct
     
-    for n in arr:
-        if target - n < 1:
-            continue
+        # for each n in arr, return combination_sum(arr, target - n)
+    
+        # enumerate, don't generate
 
-        result += combination_sum(arr, target - n)
+        if target not in self.state:
+            result = 0
+            if target == 1:
+                if target not in arr:
+                    return 0
+                result = 1
+            else:
+                if target in arr:
+                    result = 1
+                
+                for n in arr:
+                    if target - n < 1:
+                        continue
+            
+                    result += self.combination_sum(arr, target - n)
+    
+            self.state[target] = result
 
-    return result
-
+        return self.state[target]
 
 
 class MyTest(unittest.TestCase):
     def test1(self):
+        soln = Solution()
         arr = [1, 2, 3]
         target = 4
-        self.assertEqual(7, combination_sum(arr, target))
+        self.assertEqual(7, soln.combination_sum(arr, target))
 
     def test2(self):
+        soln = Solution()
         arr = [1, 2]
         target = 2
-        self.assertEqual(2, combination_sum(arr, target))
+        self.assertEqual(2, soln.combination_sum(arr, target))
 
         
