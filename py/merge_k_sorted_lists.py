4bd273083b48350ffb0d17af379581af2e4bb7e9
Doug Tomm <dougtomm@Dougs-MBP.hsd1.ca.comcast.net>
2022-08-18 19:16:43 -0700

    recast using actual linked lists and ListNode objects

--- py/merge_k_sorted_lists.py
+++ py/merge_k_sorted_lists.py
@@ -7,25 +7,57 @@ import unittest
 from pprint import pprint
 
 
+# their definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+    # def __str__(self):
+    #     return "%s" % self.val
+    #
+    # def __repr__(self):
+    #     return str(self)
+
+
 class ListState(object):
     def __init__(self, lst):
-        self.ptr = 0
+        self.ptr = lst
         self.lst = lst
 
     def valid(self):
-        return self.ptr < len(self.lst)
+        return bool(self.ptr)
 
     def current_item(self):
-        return self.lst[self.ptr]
+        return self.ptr.val
 
     def increment(self):
-        self.ptr += 1
+        if self.ptr:
+            self.ptr = self.ptr.next
 
     def __str__(self):
-        return "{%s - %s}" % (str(self.lst), self.ptr)
+        return "{%s - %s}" % (ll_to_string(self.lst), self.ptr.val if self.ptr else "<>")
 
     def __repr__(self):
         return str(self)
+
+
+def to_linked_list(l):
+    prev = None
+    for x in reversed(l):
+        n = ListNode(x, prev)
+        prev = n
+    return prev
+
+
+def ll_to_string(ll):
+    real = []
+    n = ll
+    while n:
+        real.append(n.val)
+        n = n.next
+
+    return str(real)
 
 
 class Solution:
@@ -60,30 +92,46 @@ class MyTest(unittest.TestCase):
     def setUp(self) -> None:
         self.s = Solution()
 
+    def test_ll_1(self):
+        lst = [1, 2, 3, 4]
+        ll = to_linked_list(lst)
+        self.assertEqual("[1, 2, 3, 4]", ll_to_string(ll))
+
+    def test_ll_2(self):
+        lst = [1]
+        ll = to_linked_list(lst)
+        self.assertEqual("[1]", ll_to_string(ll))
+
+    def test_ll_3(self):
+        lst = []
+        ll = to_linked_list(lst)
+        self.assertEqual("[]", ll_to_string(ll))
+
     def test_list_state_1(self):
-        l = [1]
+        l = to_linked_list([1])
         ls = ListState(l)
         self.assertTrue(ls.valid())
         ls.increment()
         self.assertFalse(ls.valid())
 
     def test_list_state_2(self):
-        l = []
+        l = to_linked_list([])
         ls = ListState(l)
         self.assertFalse(ls.valid())
 
     def test_list_state_3(self):
-        l = [1, 2]
+        l = to_linked_list([1, 2])
         ls = ListState(l)
         self.assertTrue(ls.valid())
-        self.assertEqual(l[0], ls.current_item())
+        self.assertEqual(1, ls.current_item())
         ls.increment()
-        self.assertEqual(l[1], ls.current_item())
+        self.assertEqual(2, ls.current_item())
         self.assertTrue(ls.valid())
 
     def test_merge_1(self):
         lists = [[1, 4, 5], [1, 3, 4], [2, 6]]
 
+        lists = map(lambda x: to_linked_list(x), lists)
         self.assertEqual([1, 1, 2, 3, 4, 4, 5, 6], self.s.mergeKLists(lists))
 
     def test_merge_2(self):
