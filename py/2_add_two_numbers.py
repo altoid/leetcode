56dcfabe7797d682d2dd42475f6f711054c6923c
Doug Tomm <dougtomm@Dougs-MBP.hsd1.ca.comcast.net>
2022-09-10 16:46:03 -0700

    submitted

--- py/2_add_two_numbers.py
+++ py/2_add_two_numbers.py
@@ -19,20 +19,31 @@ def to_linked_list(l):
     return prev
 
 
+def ll_to_list(ll):
+    real = []
+    n = ll
+    while n:
+        real.append(n.val)
+        n = n.next
+
+    return real
+
+
 def solution(l1, l2):
     carry = 0
-    result = []
+    head = tail = None
+    p1 = l1
+    p2 = l2
 
-    i = 0
     while True:
-        if i >= len(l1) and i >= len(l2):
+        if not p1 and not p2:
             break
 
         digit = 0
-        if i < len(l1):
-            digit += l1[i]
-        if i < len(l2):
-            digit += l2[i]
+        if p1:
+            digit += p1.val
+        if p2:
+            digit += p2.val
         digit += carry
         if digit > 9:
             digit = digit - 10
@@ -40,14 +51,23 @@ def solution(l1, l2):
         else:
             carry = 0
 
-        result.append(digit)
+        node = ListNode(digit)
+        if not head:
+            head = tail = node
+        else:
+            tail.next = node
+            tail = tail.next
 
-        i += 1
+        if p1:
+            p1 = p1.next
+        if p2:
+            p2 = p2.next
 
     if carry:
-        result.append(carry)
+        node = ListNode(carry)
+        tail.next = node
 
-    return result
+    return head
 
 
 class MyTest(unittest.TestCase):
@@ -55,10 +75,16 @@ class MyTest(unittest.TestCase):
         l1 = [2, 4, 3]
         l2 = [5, 6, 4]
         expecting = [7, 0, 8]
-        self.assertEqual(expecting, solution(l1, l2))
+        ll1 = to_linked_list(l1)
+        ll2 = to_linked_list(l2)
+        result = solution(ll1, ll2)
+        self.assertEqual(expecting, ll_to_list(result))
 
     def test2(self):
         l1 = [9, 9, 9, 9, 9, 9, 9]
         l2 = [9, 9, 9, 9]
         expecting = [8, 9, 9, 9, 0, 0, 0, 1]
-        self.assertEqual(expecting, solution(l1, l2))
+        ll1 = to_linked_list(l1)
+        ll2 = to_linked_list(l2)
+        result = solution(ll1, ll2)
+        self.assertEqual(expecting, ll_to_list(result))
