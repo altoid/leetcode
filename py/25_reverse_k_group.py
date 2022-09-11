5f5b4447bf3341ee2973dd55ac5896123635a1d6
Doug Tomm <dougtomm@Dougs-MBP.hsd1.ca.comcast.net>
2022-09-10 23:31:43 -0700

    submitted.

--- py/25_reverse_k_group.py
+++ py/25_reverse_k_group.py
@@ -1,42 +1,57 @@
 #!/usr/bin/env python
 
 import unittest
-import llutils
+from llutils import to_linked_list, ll_to_list, tail, reverse
 from pprint import pprint
 
-# idea - keep 4 pointers:  one for each of the nodes we're swapping and one each for the nodes
-# immediately before and after the pair.
 
+# idea - keep 4 pointers:  head and tail of the group we are reversing and one each for the nodes
+# immediately before and after the group
 
-def solution(head):
+def solution(head, k):
     if head is None:
         return head
 
-    if head.next is None:
+    if k == 1:
         return head
 
-    # list has at least 2 items
+    newhead = head
+    predecessor = None
+    head_of_group = head
+    tail_of_group = head_of_group
+    i = 0
+    while i < k - 1:
+        if not tail_of_group:
+            break
+        tail_of_group = tail_of_group.next
+        i += 1
 
-    p0 = None
-    p1 = head
-    p2 = head.next
-    newhead = p2
-    p3 = p2.next
+    if tail_of_group:
+        newhead = tail_of_group
 
-    while p2:
-        if p0:
-            p0.next = p2
-        p1.next = p3
-        p2.next = p1
+    while tail_of_group:
+        successor_of_group = tail_of_group.next
+        if predecessor:
+            predecessor.next = None
+        tail_of_group.next = None
 
-        p0 = p1
-        p1 = p2 = p3 = None
-        if p0:
-            p1 = p0.next
-        if p1:
-            p2 = p1.next
-        if p2:
-            p3 = p2.next
+        head_of_group = reverse(head_of_group)
+        tail_of_group = tail(head_of_group)
+
+        tail_of_group.next = successor_of_group
+        if predecessor:
+            predecessor.next = head_of_group
+
+        predecessor = tail_of_group
+        head_of_group = predecessor.next
+
+        tail_of_group = head_of_group
+        i = 0
+        while i < k - 1:
+            if not tail_of_group:
+                break
+            tail_of_group = tail_of_group.next
+            i += 1
 
     return newhead
 
@@ -45,48 +60,76 @@ class MyTest(unittest.TestCase):
     def test1(self):
         l = [1, 2, 3, 4]
         expected = [2, 1, 4, 3]
-        ll = llutils.to_linked_list(l)
-        result = solution(ll)
-        self.assertEqual(expected, llutils.ll_to_list(result))
+        ll = to_linked_list(l)
+        result = solution(ll, 2)
+        self.assertEqual(expected, ll_to_list(result))
 
     def test2(self):
         l = [1]
         expected = [1]
-        ll = llutils.to_linked_list(l)
-        result = solution(ll)
-        self.assertEqual(expected, llutils.ll_to_list(result))
+        ll = to_linked_list(l)
+        result = solution(ll, 2)
+        self.assertEqual(expected, ll_to_list(result))
 
     def test3(self):
         l = [1, 2, 3, 4, 5]
         expected = [2, 1, 4, 3, 5]
-        ll = llutils.to_linked_list(l)
-        result = solution(ll)
-        self.assertEqual(expected, llutils.ll_to_list(result))
+        ll = to_linked_list(l)
+        result = solution(ll, 2)
+        self.assertEqual(expected, ll_to_list(result))
 
     def test4(self):
         l = [1, 2]
         expected = [2, 1]
-        ll = llutils.to_linked_list(l)
-        result = solution(ll)
-        self.assertEqual(expected, llutils.ll_to_list(result))
+        ll = to_linked_list(l)
+        result = solution(ll, 2)
+        self.assertEqual(expected, ll_to_list(result))
 
     def test5(self):
         l = []
         expected = []
-        ll = llutils.to_linked_list(l)
-        result = solution(ll)
-        self.assertEqual(expected, llutils.ll_to_list(result))
+        ll = to_linked_list(l)
+        result = solution(ll, 2)
+        self.assertEqual(expected, ll_to_list(result))
 
     def test6(self):
         l = [6, 2, 5, 4, 5, 1, 6]
         expected = [2, 6, 4, 5, 1, 5, 6]
-        ll = llutils.to_linked_list(l)
-        result = solution(ll)
-        self.assertEqual(expected, llutils.ll_to_list(result))
+        ll = to_linked_list(l)
+        result = solution(ll, 2)
+        self.assertEqual(expected, ll_to_list(result))
 
     def test7(self):
         l = [6, 2, 5]
         expected = [2, 6, 5]
-        ll = llutils.to_linked_list(l)
-        result = solution(ll)
-        self.assertEqual(expected, llutils.ll_to_list(result))
+        ll = to_linked_list(l)
+        result = solution(ll, 2)
+        self.assertEqual(expected, ll_to_list(result))
+
+    def test8(self):
+        l = [6, 2, 5, 4, 5, 1, 6]
+        expected = [6, 2, 5, 4, 5, 1, 6]
+        ll = to_linked_list(l)
+        result = solution(ll, 1)
+        self.assertEqual(expected, ll_to_list(result))
+
+    def test9(self):
+        l = [6, 2, 5, 4, 5, 1, 6]
+        expected = [4, 5, 2, 6, 5, 1, 6]
+        ll = to_linked_list(l)
+        result = solution(ll, 4)
+        self.assertEqual(expected, ll_to_list(result))
+
+    def test10(self):
+        l = [6, 2, 5, 4, 5, 1, 6]
+        expected = [6, 2, 5, 4, 5, 1, 6]
+        ll = to_linked_list(l)
+        result = solution(ll, 41)
+        self.assertEqual(expected, ll_to_list(result))
+
+    def test11(self):
+        l = [6, 2, 5, 4, 5, 1, 6]
+        expected = [6, 1, 5, 4, 5, 2, 6]
+        ll = to_linked_list(l)
+        result = solution(ll, 7)
+        self.assertEqual(expected, ll_to_list(result))
