ee8d53f8692eb55c744ee0bf6b5cc6dbb1efccfe
Doug Tomm <dougtomm@Dougs-MBP.hsd1.ca.comcast.net>
2022-09-23 17:03:12 -0700

    tested the shit out of it, seems to work

--- py/sliding_window_max_239.py
+++ py/sliding_window_max_239.py
@@ -15,45 +15,58 @@ def solution_bruteforce(arr, k):
     return result
 
 
-def put_in_deque(d, n):
+def put_in_deque(d, pair):
     if len(d) == 0:
-        d.append(n)
-    elif n >= d[-1]:
-        d.append(n)
+        d.append(pair)
+    elif pair[1] >= d[-1][1]:
+        d.append(pair)
     else:
-        while d[0] < n:
+        while d[0][1] < pair[1]:
             d.popleft()
-        d.appendleft(n)
+        d.appendleft(pair)
 
 
 def solution_deque(arr, k):
     d = deque()
     result = []
+    arr_enumerated = list(enumerate(arr))  # (i, v)
 
     for i in range(k):
-        put_in_deque(d, arr[i])
-    result.append(d[-1])
+        put_in_deque(d, arr_enumerated[i])
+    result.append(d[-1][1])
 
-    for i in range(k, len(arr)):
-        if arr[i - k] == d[-1]:
-            d.pop()
-        put_in_deque(d, arr[i])
-        result.append(d[-1])
+    for i in range(k, len(arr_enumerated)):
+        try:
+            d.remove((i - k, arr_enumerated[i - k][1]))
+        except ValueError:
+            pass
+
+        put_in_deque(d, arr_enumerated[i])
+        result.append(d[-1][1])
 
     return result
 
 if __name__ == '__main__':
     # arr = [1, 3, -1, -3, 2, 3, 6, 7]
 
-    arr = [random.randint(-5, 10) for _ in range(8)]
-    k = random.randint(1, len(arr) - 1)
-    deque_result = solution_deque(arr, k)
-    bf_result = solution_bruteforce(arr, k)
+    i = 1
+    while True:
+        arr = [random.randint(-10000, 10000) for _ in range(1, 10 ** 2)]
+        k = random.randint(1, len(arr) - 1)
+        deque_result = solution_deque(arr, k)
+        bf_result = solution_bruteforce(arr, k)
 
-    print("arr = %s" % arr)
-    print("k = %s" % k)
-    print("test = %s" % deque_result)
-    print("expecting = %s" % bf_result)
+        if bf_result != deque_result:
+            print("arr = %s" % arr)
+            print("k = %s" % k)
+            print("expecting = %s" % bf_result)
+            print("test = %s" % deque_result)
+            break
+
+        if i % 1000 == 0:
+            print("ran %s tests" % i)
+
+        i += 1
 
 
 class MyTest(unittest.TestCase):
@@ -61,4 +74,10 @@ class MyTest(unittest.TestCase):
         arr = [8, 4, 9, 1, 1, -1, -2, -1]
         k = 3
         expecting = [9, 9, 9, 1, 1, -1]
+        self.assertEqual(expecting, solution_deque(arr, k))
+
+    def test_2(self):
+        arr = [10, 8, 9, 2, 8, -5, -1, -3]
+        k = 5
+        expecting = [10, 9, 9, 8]
         self.assertEqual(expecting, solution_deque(arr, k))
