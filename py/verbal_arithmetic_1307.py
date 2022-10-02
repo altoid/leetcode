a82c746b2d778bca3ad179c3baaa6a4178ab048f
Doug Tomm <dougtomm@Dougs-MBP.hsd1.ca.comcast.net>
2022-10-02 01:00:43 -0700

    dog slow but works

--- py/verbal_arithmetic_1307.py
+++ py/verbal_arithmetic_1307.py
@@ -7,7 +7,8 @@ from pprint import pprint
 import random
 from itertools import permutations
 
-def solution(addends, result):
+
+def solution_godawful(addends, result):
     # find all the unique letters
     mashup = ''.join(addends) + result
     unique_letters = ''.join(sorted(list(set(mashup))))
@@ -15,21 +16,36 @@ def solution(addends, result):
     # find the first letters of all of the addends and result
     first_letters = list(set([x[0] for x in addends + [result]]))
     pprint(first_letters)
+    first_letter_index = {}
+    for l in first_letters:
+        first_letter_index[l] = unique_letters.index(l)
 
     digits = [str(x) for x in range(10)]
-
+    pprint(first_letter_index)
     for p in permutations(digits, len(unique_letters)):
-        break
-
         # map each permutation to the digits.
+        usable = True
+        for k, v in first_letter_index.items():
+            if p[v] == '0':
+                usable = False
+                break
+
+        if not usable:
+            continue
+
         s = ''.join(p)
         ttable = str.maketrans(unique_letters, s)
         sum_int = int(result.translate(ttable))
         addends_int = [int(x.translate(ttable)) for x in addends]
         if sum(addends_int) == sum_int:
             print("###########################")
+            print(unique_letters)
+            print(s)
             print(addends, addends_int)
             print(result, sum_int)
+            return True
+
+    return False
 
 
 if __name__ == '__main__':
@@ -41,5 +57,34 @@ class MyTest(unittest.TestCase):
         addends = ["SEND", "MORE"]
         sum = "MONEY"
 
-        solution(addends, sum)
+        self.assertTrue(solution_godawful(addends, sum))
 
+    def test_2(self):
+        addends = ["SHIRT", "TSHIRT"]
+        sum = "CLOTHES"
+
+        self.assertTrue(solution_godawful(addends, sum))
+
+    def test_3(self):
+        addends = ["SKIRT", "TSHIRT"]
+        sum = "CLOTHES"
+
+        self.assertTrue(solution_godawful(addends, sum))
+
+    def test_4(self):
+        addends = ["GHANA", "GABON", "BHUTAN"]
+        sum = "ALBANIA"
+
+        self.assertTrue(solution_godawful(addends, sum))
+
+    def test_5(self):
+        addends = ["SEAL", "SNAIL", "MONKEY"]
+        sum = "ANIMALS"
+
+        self.assertTrue(solution_godawful(addends, sum))
+
+    def test_6(self):
+        addends = ["RICH", "POOR", "HAPPY"]
+        sum = "PEOPLE"
+
+        self.assertTrue(solution_godawful(addends, sum))
