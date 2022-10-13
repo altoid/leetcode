76b00d57b4bf9c37fdf1c276f202b70a17315da9
Doug Tomm <dougtomm@Dougs-MBP.hsd1.ca.comcast.net>
2022-10-13 00:29:38 -0700

    given test cases are passing.

---
+++ py/restore_ip_addresses_93.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python
+
+# https://leetcode.com/problems/restore-ip-addresses/
+
+import unittest
+from pprint import pprint
+import random
+
+
+# rules
+#
+# each octet [0..255]
+# no leading 0
+# all digits used
+
+# use backtracking
+
+
+def is_valid(octet):
+    if not bool(octet):
+        return False
+
+    b = int(octet)
+    if b > 255:
+        return False
+
+    if b == 0 and len(octet) > 1:
+        return False
+
+    if b > 0 and octet[0] == '0':
+        return False
+
+    return True
+
+
+def helper(s, level):
+    if not s:
+        return None
+
+    if level == 1:
+        if is_valid(s):
+            return [[s]]
+        return None
+
+    result = []
+    for i in range(1, 4):
+        if len(s) < i:
+            continue
+
+        octet_str = s[:i]
+        if not is_valid(octet_str):
+            continue
+        restof = s[i:]
+        partial = helper(restof, level - 1)
+        if not partial:
+            continue
+        for p in partial:
+            result.append(p + [octet_str])
+    return result
+
+
+def solution(s):
+    result = helper(s, 4)
+    answer = set()
+    for r in result:
+        rr = '.'.join(r[::-1])
+        answer.add(rr)
+    return answer
+
+
+if __name__ == '__main__':
+    pass
+
+
+class MyTest(unittest.TestCase):
+    def test_level_1(self):
+        self.assertIsNone(helper("", 1))
+        self.assertIsNone(helper("00", 1))
+        self.assertIsNone(helper("012", 1))
+        self.assertIsNone(helper("258", 1))
+
+        self.assertIsNotNone(helper("123", 1))
+        self.assertIsNotNone(helper("0", 1))
+        self.assertIsNotNone(helper("10", 1))
+        self.assertIsNotNone(helper("255", 1))
+
+        pprint(helper("11", 1))
+
+    def test_level_2_1(self):
+        result = helper("123", 2)
+
+        answer = set()
+        for r in result:
+            rr = '.'.join(r[::-1])
+            answer.add(rr)
+
+        expected = {'1.23', '12.3'}
+        self.assertEqual(expected, answer)
+
+    def test_level_2_2(self):
+        result = helper("120", 2)
+
+        answer = set()
+        for r in result:
+            rr = '.'.join(r[::-1])
+            answer.add(rr)
+
+        expected = {'1.20', '12.0'}
+        self.assertEqual(expected, answer)
+
+    def test_1(self):
+        result = set(solution("25525511135"))
+        self.assertEqual({"255.255.11.135", "255.255.111.35"}, result)
+
+    def test_2(self):
+        result = set(solution("101023"))
+        expecting = {"1.0.10.23", "1.0.102.3", "10.1.0.23", "10.10.2.3", "101.0.2.3"}
+        self.assertEqual(expecting, result)
+
+    def test_3(self):
+        result = set(solution("0000"))
+        expecting = {"0.0.0.0"}
+        self.assertEqual(expecting, result)
