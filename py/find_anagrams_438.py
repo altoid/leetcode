d2356f91353f2aafd0c3420f9a247b90f0e3fe70
Doug Tomm <dougtomm@Dougs-MBP.hsd1.ca.comcast.net>
2022-10-28 01:20:57 -0700

    submitted

--- py/find_anagrams_438.py
+++ py/find_anagrams_438.py
@@ -32,18 +32,30 @@ def solution(s, p):
         if sdict == pdict:
             result.append(x)
 
+        if x + width >= len(s):
+            break
         c = s[x]
         sdict[c] -= 1
         if sdict[c] == 0:
             del sdict[c]
 
         c = s[x + width]
+        if c not in pdict:
+            x = x + width + 1
+            sdict = {}
+            for k in s[x:x+len(p)]:
+                if k not in sdict:
+                    sdict[k] = 0
+                sdict[k] += 1
+
+            continue
+
         if c not in sdict:
             sdict[c] = 0
         sdict[c] += 1
 
         x += 1
-        
+
     return result
 
 
@@ -61,3 +73,18 @@ class MyTest(unittest.TestCase):
         r = solution(s, p)
         print(r)
         self.assertEqual([0, 6], sorted(r))
+
+    def test_2(self):
+        s = "abab"
+        p = "ab"
+        r = solution(s, p)
+        print(r)
+        self.assertEqual([0, 1, 2], sorted(r))
+
+    def test_3(self):
+        s = "abcxabc"
+        p = "abc"
+        r = solution(s, p)
+        print(r)
+        self.assertEqual([0, 4], sorted(r))
+
