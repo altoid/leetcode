0834a9ba3d95d10aff54ddfea3c533e092163941
Doug Tomm <dougtomm@Dougs-MBP.hsd1.ca.comcast.net>
2022-10-27 22:18:40 -0700

    works but too slow

--- py/triangle_120.py
+++ py/triangle_120.py
@@ -9,41 +9,54 @@ import random
 
 def helper(arr, row, stack):
     if row == len(arr):
-        print("1. ", [stack[-1]])
-        yield [stack[-1]]
+        yield
         return
 
     for i in range(stack[row - 1], stack[row - 1] + 2):
         stack.append(i)
-        for r in helper(arr, row + 1, stack):
-            print("2. ", [stack[-1]] + r)
-            yield [stack[-1]] + r
+        for _ in helper(arr, row + 1, stack):
+            yield
         stack.pop()
 
 
-def sums(arr):
+def paths(arr):
     stack = [0]
     row = 1
     for i in range(stack[row - 1], stack[row - 1] + 2):
         stack.append(i)
         for _ in helper(arr, row + 1, stack):
-            print("3. ", stack)
-            yield stack
+            result = []
+            p = 0
+            while p < len(stack):
+                result.append(arr[p][stack[p]])
+                p += 1
+            yield result
         stack.pop()
 
 
 def solution(arr):
-    result = min(list(sums(arr)))
-    return result
+    if len(arr) == 1:
+        return arr[0][0]
+    return min(map(sum, paths(arr)))
 
 
 if __name__ == '__main__':
     arr = [[2], [3, 4], [6, 5, 7], [4, 1, 8, 3], [11, 22, 33, 44, 55]]
-    for x in sums(arr):
+    for x in paths(arr):
         print(x)
+
+    print(min(map(sum, paths(arr))))
 
 
 class MyTest(unittest.TestCase):
     def test_1(self):
         arr = [[2], [3, 4], [6, 5, 7], [4, 1, 8, 3]]
         self.assertEqual(11, solution(arr))
+
+    def test_2(self):
+        arr = [[-10]]
+        self.assertEqual(-10, solution(arr))
+
+    def test_3(self):
+        arr = [[-10], [1, -1]]
+        self.assertEqual(-11, solution(arr))
