6f4d45b6cb37413560134253cbb7f696144cdf9a
Doug Tomm <dougtomm@Dougs-MBP.hsd1.ca.comcast.net>
2022-10-27 22:00:52 -0700

    checkpoint

--- py/triangle_120.py
+++ py/triangle_120.py
@@ -9,36 +9,41 @@ import random
 
 def helper(arr, row, stack):
     if row == len(arr):
-        result = []
-        i = 0
-        while i < len(stack):
-            result.append(arr[i][stack[i]])
-            i += 1
-        print(stack, result)
+        print("1. ", [stack[-1]])
+        yield [stack[-1]]
         return
 
     for i in range(stack[row - 1], stack[row - 1] + 2):
         stack.append(i)
-        helper(arr, row + 1, stack)
+        for r in helper(arr, row + 1, stack):
+            print("2. ", [stack[-1]] + r)
+            yield [stack[-1]] + r
         stack.pop()
 
 
-def solution(arr):
+def sums(arr):
     stack = [0]
     row = 1
     for i in range(stack[row - 1], stack[row - 1] + 2):
         stack.append(i)
-        helper(arr, row + 1, stack)
+        for _ in helper(arr, row + 1, stack):
+            print("3. ", stack)
+            yield stack
         stack.pop()
 
 
+def solution(arr):
+    result = min(list(sums(arr)))
+    return result
+
+
 if __name__ == '__main__':
-    arr = [[2], [3, 4], [6, 5, 7], [4, 1, 8, 3]]
-    solution(arr)
+    arr = [[2], [3, 4], [6, 5, 7], [4, 1, 8, 3], [11, 22, 33, 44, 55]]
+    for x in sums(arr):
+        print(x)
 
 
 class MyTest(unittest.TestCase):
     def test_1(self):
         arr = [[2], [3, 4], [6, 5, 7], [4, 1, 8, 3]]
         self.assertEqual(11, solution(arr))
-
