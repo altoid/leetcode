bf709fda28200f8a323ac03ad33e97524d12d946
Doug Tomm <dougtomm@Dougs-MBP.hsd1.ca.comcast.net>
2022-11-27 00:05:01 -0800

    can do this without modifying the array.  processing each level should not be n ** 2.

--- py/rain_water_42.py
+++ py/rain_water_42.py
@@ -13,11 +13,11 @@ import random
 
 # keep increasing levels.
 
-def next_hole_at_level(arr, level):
+def next_hole_at_level(arr, level, start):
     """
     give me the array slice of the next slot we can fill at this level.  return None if no candidates.
     """
-    i = 0
+    i = start
     while i < len(arr) and arr[i] <= level:
         i += 1
 
@@ -47,15 +47,14 @@ def next_hole_at_level(arr, level):
 def fill_level(arr, level):
     result = 0
 
-    slice = next_hole_at_level(arr, level)
+    slice = next_hole_at_level(arr, level, 0)
     while slice is not None:
         i, j = slice
         delta = j - i
-        arr = arr[:i] + [level + 1] * delta + arr[j:]
         result += delta
-        slice = next_hole_at_level(arr, level)
+        slice = next_hole_at_level(arr, level, j)
 
-    return result, arr
+    return result
 
 
 def solution(arr):
@@ -67,7 +66,7 @@ def solution(arr):
     maxlevel = max(arr)
 
     for l in range(minlevel, maxlevel):
-        result, arr = fill_level(arr, l)
+        result = fill_level(arr, l)
         total += result
 
     return total
