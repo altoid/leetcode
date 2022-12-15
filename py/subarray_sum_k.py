2f2a7d85f91e172fe07b699304cca9375c58a7d7
Doug Tomm <dougtomm@Dougs-MBP.hsd1.ca.comcast.net>
2022-12-14 22:58:10 -0800

    checkpoint

---
+++ py/subarray_sum_k.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+# url for problem here
+
+import unittest
+from pprint import pprint
+import random
+
+
+def solution(nums, k):
+    prefix_sums = []
+    partial_sum = 0
+    for i in range(len(nums)):
+        partial_sum += nums[i]
+        prefix_sums.append(partial_sum)
+
+    prefix_sum_to_i = {}
+    for i in range(len(prefix_sums)):
+        if prefix_sums[i] not in prefix_sum_to_i:
+            prefix_sum_to_i[prefix_sums[i]] = []
+        prefix_sum_to_i[prefix_sums[i]].append(i)
+
+    additive_inverses = {s: s - k for s in prefix_sums}
+
+    result = 0
+
+    pprint(prefix_sum_to_i)
+    pprint(prefix_sums)
+    for i in range(len(prefix_sums)):
+        inv = additive_inverses[prefix_sums[i]]
+        if inv in prefix_sum_to_i:
+            candidates = prefix_sum_to_i[inv]
+            candidates = list(filter(lambda x: x < i, candidates))
+            result += len(candidates)
+        if prefix_sums[i] == k:
+            result += 1
+
+    return result
+
+
+if __name__ == '__main__':
+    pass
+
+
+class MyTest(unittest.TestCase):
+    def test_1(self):
+        nums = [1, -1, 1, -1, 1, -1, 1, -1, 1, -1, 1, -1, 1, -1, 1, -1, 1, -1, 1, -1, 1]
+        k = 0
+        print(solution(nums, k))
