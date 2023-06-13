00ee7d50fb84168cc138a885dd3443c9377ea080
Doug Tomm <dougtomm@Dougs-MBP.hsd1.ca.comcast.net>
2023-06-13 00:14:26 -0700

    accepted

---
+++ py/copy_list_random_pointer_138.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+import unittest
+from pprint import pprint
+
+
+class Node:
+    def __init__(self, x: int, nxt=None, random=None):
+        self.val = int(x)
+        self.next = nxt
+        self.random = random
+
+
+def solution(head):
+    if not head:
+        return None
+
+    id_mappings = {}
+    newhead = Node(head.val)
+    to_copy = head.next
+    dest = newhead
+    while to_copy is not None:
+        new_node = Node(to_copy.val)
+        id_mappings[id(to_copy)] = id(new_node)
+        dest.next = new_node
+        dest = new_node
+
+    p = head
+    np = newhead
+    while True:
+        if not p:
+            break
+
+        np.random = id_mappings[p.random]
+        p = p.next
+        np = np.next
+
+    return newhead
+
+
+if __name__ == '__main__':
+    pass
+
+
+class MyTest(unittest.TestCase):
+    def test_1(self):
+        pass
