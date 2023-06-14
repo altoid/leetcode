dd3a555c8756eb1b31c74843a6dd062a3b6fa1bc
Doug Tomm <dougtomm@Dougs-MBP.hsd1.ca.comcast.net>
2023-06-13 18:59:05 -0700

    accepted

--- py/copy_list_random_pointer_138.py
+++ py/copy_list_random_pointer_138.py
@@ -16,14 +16,17 @@ def solution(head):
         return None
 
     id_mappings = {}
+
     newhead = Node(head.val)
     to_copy = head.next
     dest = newhead
+    id_mappings[head] = newhead
     while to_copy is not None:
         new_node = Node(to_copy.val)
-        id_mappings[id(to_copy)] = id(new_node)
+        id_mappings[to_copy] = new_node
         dest.next = new_node
         dest = new_node
+        to_copy = to_copy.next
 
     p = head
     np = newhead
@@ -31,7 +34,9 @@ def solution(head):
         if not p:
             break
 
-        np.random = id_mappings[p.random]
+        if p.random in id_mappings:
+            np.random = id_mappings[p.random]
+
         p = p.next
         np = np.next
 
