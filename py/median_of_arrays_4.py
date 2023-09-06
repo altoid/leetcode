bf8b95327b917ab2989e602aaeeb6e5873e81054
Doug Tomm <dougtomm@Dougs-MBP.hsd1.ca.comcast.net>
2023-09-06 16:28:48 -0700

    revised partition algorithm; added tests.

--- py/median_of_arrays_4.py
+++ py/median_of_arrays_4.py
@@ -8,11 +8,8 @@
 # complexity should be O(log (m+n)).
 
 
-
 import unittest
 import random
-from binary_search import binary_search
-
 
 def simple_median(a):
     len_a = len(a)
@@ -35,95 +32,32 @@ def array_as_str(a, **kwargs):
     return '%s [%s]' % (' '.join(map(str, a)), simple_median(a))
 
 
-def partition(a):
-    p = len(a) // 2
-    if len(a) % 2 == 0:
-        p -= 1
-    return p
+def partition(a, b):
+    """
+    return the number of items in a and b that are present in the left partition of a and b.
+    """
+    # len(a) <= len(b)
 
+    h = (len(a) + len(b)) // 2
 
-def adjust(a, partition_a, median_a, b, partition_b, median_b):
-    # b is the shorter array
-    if median_b > median_a:
-        # binary search in the left part of b for the leftmost occurrence of the largest value
-        # that is <= the median
-        new_part_b = binary_search.search_largest_less_than(b[:(partition_b + 1)], median_b)
-        if new_part_b is None:
-            # oh, boy.  if we can't locate this value then the partition is already 0.  instead
-            # we have to move partition_a to the right by half the length of b.
-            print("######################################")
-            new_part_a = partition_a + len(b) // 2
-            return new_part_a, new_part_b
+    # a_left is the number of items from a that are in the left partition.  always >= 1.
+    # b_left is the number of items from b that are in the left partition.  could be 0.
+    a_left = (len(a) + 1) // 2
+    b_left = h - a_left
 
-        new_part_b -= 1  # since the search gave us the left endpoint of the right part
-    else:
-        # binary search in the right part of b for the rightmost occurrence of the smallest value that is >= the median
-        new_part_b = binary_search.search_smallest_greater_than(b[(partition_b + 1):], median_b)
-        new_part_b += (partition_b + 1)  # add back the true index of the median
-
-    delta = new_part_b - partition_b
-
-    new_part_a = partition_a - delta
-    return new_part_a, new_part_b
+    return a_left, b_left
 
 
 def find_median(arr1, arr2):
-    # take a to be the longer one
+    # take a to be the shorter one
     a = arr1
     b = arr2
-    if len(arr2) > len(arr1):
+    if len(arr2) < len(arr1):
         a, b = b, a
-
-    median_a = simple_median(a)
-    median_b = simple_median(b)
-
-    parity_len_a = len(a) % 2
-    parity_len_b = len(b) % 2
-
-    # partition_x is the rightmost index of the left partition
-    partition_a = partition(a)
-    partition_b = partition(b)
-
-    left_size = partition_a + partition_b + 2
-    right_size = len(a) + len(b) - left_size
-
-    print("left_size = %s, right_size = %s" % (left_size, right_size))
-
-    # if the array with the larger median is odd length, subtract 1 from the partition
-    if median_b > median_a:
-        if parity_len_b == 1 and right_size - left_size > 1:
-            partition_b -= 1
-    if median_a > median_b:
-        if parity_len_a == 1 and right_size - left_size > 1:
-            partition_a -= 1
-
-    print(array_as_str(sorted(a + b)))
-
-    print(array_as_str(a, partition=partition_a))
-    print(array_as_str(b, partition=partition_b))
-
-    left_max = max(a[partition_a], b[partition_b])
-    right_min = min(a[partition_a + 1], b[partition_b + 1])
-
-    while left_max > right_min:
-        partition_a, partition_b = adjust(a, partition_a, median_a, b, partition_b, median_b)
-
-        print(array_as_str(a, partition=partition_a))
-        print(array_as_str(b, partition=partition_b))
-
-        left_max = max(a[partition_a], b[partition_b])
-        right_min = min(a[partition_a + 1], b[partition_b + 1])
-
-    if parity_len_a != parity_len_b:
-        result = left_max
-    else:
-        result = (left_max + right_min) / 2.0
-
-    return result
 
 
 def generate_array():
-    length = random.randint(2, 15)
+    length = random.randint(1, 15)
     return sorted([random.randint(10, 40) for _ in range(length)])
 
 
@@ -144,23 +78,42 @@ if __name__ == '__main__':
     print("b = %s" % array_as_str(b))
     print("combo = %s" % array_as_str(sorted(a + b)))
 
-    result = find_median(a, b)
     compare = true_median(a, b)
-    print("found median = %s" % result)
     print("true median = %s" % compare)
 
 
+class TestPartition(unittest.TestCase):
+
+    def test_partition_1(self):
+        a = [1]
+        b = [5]
+        a_left, b_left = partition(a, b)
+        self.assertEqual(1, a_left)
+        self.assertEqual(0, b_left)
+
+    def test_partition_2(self):
+        a = [1]
+        b = [5, 6]
+        a_left, b_left = partition(a, b)
+        self.assertEqual(1, a_left)
+        self.assertEqual(0, b_left)
+
+    def test_partition_3(self):
+        a = [1, 2]
+        b = [5, 6]
+        a_left, b_left = partition(a, b)
+        self.assertEqual(1, a_left)
+        self.assertEqual(1, b_left)
+
+    def test_partition_4(self):
+        a = [1, 2]
+        b = [5, 6, 7]
+        a_left, b_left = partition(a, b)
+        self.assertEqual(1, a_left)
+        self.assertEqual(1, b_left)
+
+
 class TestMedian(unittest.TestCase):
-
-    def test_partition(self):
-        a = [1, 2, 3, 4, 5]
-        p = partition(a)
-        self.assertEqual(p, 2)
-
-        a = [1, 2, 3, 4, 5, 6]
-        p = partition(a)
-        self.assertEqual(p, 2)
-
     def test_median_1(self):
         a = [14, 16, 20, 21, 27, 29, 37, 38]
         b = [13, 17, 21, 22, 23, 29, 31, 31, 32, 35, 35]
