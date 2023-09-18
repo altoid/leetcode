686e013f266c02b802865af384caeba9ce63ee81
Doug Tomm <dougtomm@Dougs-MBP.hsd1.ca.comcast.net>
2023-09-17 21:13:55 -0700

    passes tests

---
+++ py/buy_sell_stock_122.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+
+import unittest
+from pprint import pprint
+import random
+
+
+def solution(prices):
+    if len(prices) < 2:
+        return 0
+
+    # keep track of all increasing windows
+    left = right = 0
+    totalprofit = 0
+    current_window_profit = 0
+
+    for i in range(1, len(prices)):
+        if prices[i] > prices[right]:
+            current_window_profit += (prices[i] - prices[right])
+            right = i
+        elif prices[i] < prices[right]:
+            left = right = i
+            totalprofit += current_window_profit
+            current_window_profit = 0
+
+    return totalprofit
+
+
+def generate_array():
+    length = random.randint(1, 10)
+    return [random.randint(1, 40) for _ in range(length)]
+
+
+if __name__ == '__main__':
+    arr = generate_array()
+    answer = solution(arr)
+    print(arr)
+    print(answer)
+
+
+class MyTest(unittest.TestCase):
+    def test_3(self):
+        arr = [7, 6, 4, 3, 1]
+        expected = 0
+        self.assertEqual(expected, solution(arr))
+
+    def test_2(self):
+        arr = [1, 2, 3, 4, 5]
+        expected = 0
+        self.assertEqual(expected, solution(arr))
+
+    def test_1(self):
+        arr = [7, 1, 5, 3, 6, 4]
+        expected = 7
+        self.assertEqual(expected, solution(arr))
